11899d1c2a75ae49e78f35e16d9b1be2ec67cad6
brianstorm2 <matt.j.parsons99@gmail.com>
2025-06-26 21:07:26 +0100

    Export Data to Excel

--- LinkedinApplicationsScrape.py
+++ LinkedinApplicationsScrape.py
@@ -6,10 +6,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import xlsxwriter
+import re
 
 #visualisations, num of application line graph dates
 #pie chart views vs applications
 #totals in spreadhseet, number of jobs
+#organisations that viewed your application
 
 def run_linkedin_scraper():
     url = "https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED"
@@ -23,7 +25,8 @@ def run_linkedin_scraper():
         )
         #last_page, know how many times to iterate
         last_page_number = int(last_page_selenium.text)  # converting last page to int
-        end_number_of_applications = (last_page_number * 10) - 10
+        end_number_of_applications = (last_page_number * 10)
+        print(end_number_of_applications)
 
         collect_job_data()
 
@@ -32,6 +35,7 @@ def run_linkedin_scraper():
 
     #iterate through each page
     for i in range(10, end_number_of_applications, 10):
+        ##go headless
         new_url = "https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED&start="+str(i)
         driver.get(new_url)
         time.sleep(3)
@@ -42,7 +46,9 @@ def collect_job_data():
     jobs = driver.find_elements(By.CSS_SELECTOR, 'div.t-roman.t-sans')
     for job in jobs:
         raw_job_text = str(job.text)
-        raw_job_text.replace('\n, Verified', '') #cleaning data
+        #data sanitisation
+        raw_job_text = re.sub(r'\n?,? Verified', '', raw_job_text)
+        raw_job_text = raw_job_text.replace('\n', '')
         job_titles.append(raw_job_text)
 
     #fetching company names
@@ -50,8 +56,39 @@ def collect_job_data():
     for company in companies:
         company_names.append(company.text)
 
+    #regex for time since application    
+    pattern = re.compile(r'(Application viewed|Applied) (\d+)(h|d|w|mo|y) ago')
+
     #fetching time since application, sort views as well
-    days_since_applying = driver.find_elements(By.CSS_SELECTOR, '.reusable-search-simple-insight__text--small')
+    time_since_applying = driver.find_elements(By.CSS_SELECTOR, '.reusable-search-simple-insight__text--small')
+    for time in time_since_applying:
+        text = time.text
+        match = pattern.match(text)
+        #if regex matches
+        if match:
+            status, value, unit = match.groups()
+            time_since_applications.append(f"{value}{unit}")
+            application_views.append('y' if status == 'Application viewed' else 'n')
+        else:
+            time_since_applications.append("unknown")
+            application_views.append("n")
+
+
+def export_data_excel():
+    workbook_name = "LinkedIn Application Data"
+    workbook = xlsxwriter.Workbook(workbook_name)
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write('A1', 'Job Title')
+    worksheet.write('B1', 'Company Name')
+    worksheet.write('C1', 'Time Since Application')
+    worksheet.write('D1', 'Application Viewed (Y/N)')
+
+    for row_number in range(2, len(job_titles)):
+        job_title_cell = 'A'+str(row_number)
+        company_name_cell = 'B'+str(row_number)
+        time_since_app_cell = 'C'+str(row_number)
+        app_viewed_cell = 'D'+str(row_number)
     
 
 options = webdriver.ChromeOptions()
@@ -60,6 +97,7 @@ driver = webdriver.Chrome(options=options)
 #establish lists that will be output to excel
 job_titles = []
 company_names = []
-days_since_applications = []
+time_since_applications = []
+application_views = []
 
 run_linkedin_scraper()
