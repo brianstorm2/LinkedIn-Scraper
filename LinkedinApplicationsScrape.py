b0476c6f600ca94f13d54745b4b40986f105b1f9
brianstorm2 <matt.j.parsons99@gmail.com>
2025-06-26 17:37:46 +0100

    Initial Edits

--- LinkedinApplicationsScrape.py
+++ LinkedinApplicationsScrape.py
@@ -11,9 +11,12 @@ driver = webdriver.Chrome(options=options)
 url = "https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED"
 driver.get(url)
 
-job_title = []
-company_name = []
-days_since_application = []
+job_titles = []
+company_names = []
+days_since_applications = []
 
-job = driver.find_elements(By.CSS_SELECTOR  , '[entity-result__title-text]')
-print(job)
+jobs = driver.find_elements(By.CSS_SELECTOR, 'div.t-roman.t-sans')
+for job in jobs:
+    raw_job_text = str(job.text)
+    raw_job_text.replace('\n, Verified', '') #cleaning data
+    job_titles.append(raw_job_text)
